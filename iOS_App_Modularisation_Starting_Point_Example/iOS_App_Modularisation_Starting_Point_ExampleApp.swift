b2eaf154c3bc24a789f59565ac6467986ae991a1
Darius Sabaliauskas <dsab@mobilepay.lt>
2022-06-06 12:47:24 +0300

    Initial commit

---
+++ iOS_App_Modularisation_Starting_Point_Example/iOS_App_Modularisation_Starting_Point_ExampleApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct iOS_App_Modularisation_Starting_Point_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
